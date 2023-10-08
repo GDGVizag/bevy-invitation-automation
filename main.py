c8291a2155b13696634d0f86c018bc9d4db786df
Sai Sampath Kumar Balivada <balivadask2000@gmail.com>
2023-10-08 19:03:21 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Handle cases only with 2 or more attendees
+
+# 1. Ask for csv file location from the user
+# 2. Place the generated json in generated folder with .side extension
+
+# Serialize the contents of base.json into a dictionary
+
+# Generate and set IDs:
+# Base ID
+# Test ID
+# Test Suite ID
+
+# Load the contents of config.json and set them
+
+# prepend start_commands.json
+
+# Generate commands for adding attendees iteratively
+
+# append end_commands.json
