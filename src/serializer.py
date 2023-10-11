8904fc559e41a4823a225af7394e10660ef3618d
yaswanth <yaswanth14333@gmail.com>
2023-10-11 19:42:15 +0530

    Added functions validate_file_format and validate_row

--- src/serializer.py
+++ src/serializer.py
@@ -61,13 +61,51 @@ class AttendeeCSVSerializer:
         self.attendees = []
         with open('./samples/attendees.csv', newline='', encoding='utf-8') as csv_file:
             csv_reader = csv.reader(csv_file, skipinitialspace=True)
-            next(csv_reader, None)
+            headers = next(csv_reader, None)
+
+            self.validate_file_format(headers)
+
             for row in csv_reader:
+                self.validate_row(row)
                 self.attendees.append({
                     'first_name': row[0],
                     'last_name': row[1],
                     'email' : row[2]
                 })
+    
+    def validate_file_format(self, headers):
+        """
+        Validates the CSV file format
+        """
+
+        #check file data
+        if(headers) == None:
+            raise ValueError('The file is empty')
+
+        #check number of columns
+        if(len(headers)) != 3:
+            raise ValueError('The file must have exactly three columns')
+        
+        #check column names
+        if headers[0] != 'first_name':
+            raise ValueError('The first column name must be `first_name`')
+        if headers[1] != 'last_name':
+            raise ValueError('The second column name must be `last_name`')
+        if headers[2] != 'email':
+            raise ValueError('The third column name must be `email`')
+
+    def validate_row(self, row):
+        """
+        Validates the row format
+        """
+
+        #check the first and last name
+        if not row[0] or not row[1]:
+            raise ValueError('The first name and last name cannot be empty')
+
+        # Check the email address
+        if not re.match(r'^.+@.+\..+$', row[2]):
+            raise ValueError('The email address is invalid')
 
     def get_attendees(self):
         """
